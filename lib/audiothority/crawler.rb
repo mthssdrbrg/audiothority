4bdca3a7f9a23f8de56b333af366a2b191ca6124
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-08-12 14:53:38 +0200

    Add basic blacklist to Crawler

--- lib/audiothority/crawler.rb
+++ lib/audiothority/crawler.rb
@@ -9,11 +9,31 @@ module Audiothority
     def each_crawled
       @dirs.each do |dir|
         dir.each_child do |path|
-          if path.readable? && path.directory? && path.children.any?
+          if consider?(path)
             yield path
           end
         end
       end
+    end
+
+    private
+
+    def consider?(path)
+      path.readable? && path.directory? && path.children.any? && !ignore?(path)
+    end
+
+    def ignore?(path)
+      blacklist.any? { |r| path.basename.to_s.match(r) }
+    end
+
+    def blacklist
+      @blacklist ||= [
+        /Various.Artists/i,
+        /\/VA/,
+        /OST/,
+        /Original.Soundtrack/i,
+        /Singles/
+      ].freeze
     end
   end
 end
