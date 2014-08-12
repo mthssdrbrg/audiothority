1477bb5fbf62cad3a4f944e09c7f2da7714e8b6c
Mathias Söderberg <mathias.soederberg@gmail.com>
2014-08-13 01:51:31 +0200

    Add `simplecov` and `coveralls`

--- Gemfile
+++ Gemfile
@@ -7,4 +7,6 @@ gem 'thor'
 
 group :test do
   gem 'rspec'
+  gem 'simplecov', require: false
+  gem 'coveralls', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,18 @@
 require 'tmpdir'
 require 'support/cli_setup'
 require 'support/interactive'
+require 'coveralls'
+require 'simplecov'
 
+if ENV.include?('TRAVIS')
+  Coveralls.wear!
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+end
+
+SimpleCov.start do
+  add_group 'Source', 'lib'
+  add_group 'Unit tests', 'spec/audiothority'
+  add_group 'Integration tests', 'spec/integration'
+end
 
 require 'audiothority'
